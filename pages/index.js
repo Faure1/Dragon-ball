571255a405f4db92c8efdf68dbaf1da581c8e862
Faure1 <104220128+Faure1@users.noreply.github.com>
2023-05-22 18:19:31 -0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 /** @format */
 
 import React, { useState } from "react";
-import { Button } from "@nextui-org/react";
+import { Button, Grid, Text,Link } from "@nextui-org/react";
 import Layout from "@/components/Layout";
 import CacilleroAsk from "@/components/CacilleroAsk";
 import Asks from "@/utils";
 export default function Home() {
   const [contadorCorrectas, setContadorCorrectas] = useState(0);
   return (
-    <Layout>
+    <section>
+      <Grid.Container>
+        <Grid justify="center" xs={12}>
+          <Text className="titulo" h1>Ciudadania y politica</Text>
+        </Grid>
+      </Grid.Container>
       {Asks.map((item) => (
         <CacilleroAsk
           setCorec={setContadorCorrectas}
@@ -19,7 +24,23 @@ export default function Home() {
           respoC={item.respoCorrecta}
         />
       ))}
-      <h1>respuestas correctas: {contadorCorrectas}</h1>
-    </Layout>
+      <Grid.Container alignItems="center">
+        <Grid xs={1}>
+          <Button
+            css={{ bg: "#025a4e", color: "white" }}
+            auto
+          >
+            <Link href="https://www.canva.com/design/DAFicccypTs/0V42zWyauGyNycbmneFoPQ/edit?utm_content=DAFicccypTs&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">
+                Volver
+            </Link>
+          </Button>
+        </Grid>
+        <Grid justify="flex-end" xs={11}>
+          <Text h1>
+            respuestas correctas: {contadorCorrectas}
+          </Text>
+        </Grid>
+      </Grid.Container>
+    </section>
   );
 }
