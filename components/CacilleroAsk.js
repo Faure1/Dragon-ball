a4cc852d791ff382b3b220ddbbd4849bc4431ec8
Faure1 <104220128+Faure1@users.noreply.github.com>
2023-05-22 18:19:57 -0300

    Add files via upload

--- components/CacilleroAsk.js
+++ components/CacilleroAsk.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import { Button, css } from "@nextui-org/react";
+import { Button, css, Grid, Text } from "@nextui-org/react";
 import React, { useState } from "react";
 import Swal from "sweetalert2";
 
@@ -36,64 +36,77 @@ export default function CacilleroAsk({
           text: "¡respuesta incorrecta!",
         });
       }
-      if (respoC == "but1"){setBot1(true)}
-      if (respoC == "but2"){setBot2(true)}
-      if (respoC == "but3"){setBot3(true)}
-      if (respoC == "but4"){setBot4(true)}
+      if (respoC == "but1") { setBot1(true) }
+      if (respoC == "but2") { setBot2(true) }
+      if (respoC == "but3") { setBot3(true) }
+      if (respoC == "but4") { setBot4(true) }
       setCont("paso");
     }
   };
   return (
     <div key={numPregunta}>
-      <div>
-        <h4>pregunta numero {numPregunta}</h4>
-      </div>
-      <div>
-        <p>{content}</p>
-      </div>
-
-      <div>
-        <div>
-          <Button
-            value={respuestas?.respoUno[1]}
-            bordered
-            color={bot1 ? "success" : "grey"}
-            name='1'
-            onClick={() => corroborador(respuestas?.respoUno)}>
-            {respuestas?.respoUno[0]}
-          </Button>
-        </div>
-        <div>
-          <Button
-            value={respuestas?.respoDos[1]}
-            bordered
-            color={bot2 ? "success" : "grey"}
-            name='2'
-            onClick={() => corroborador(respuestas?.respoDos)}>
-            {respuestas?.respoDos[0]}
-          </Button>
-        </div>
-        <div>
-          <Button
-            value={respuestas?.respoTres[1]}
-            bordered
-            color={bot3 ? "success" : "grey"}
-            name='3'
-            onClick={() => corroborador(respuestas?.respoTres)}>
-            {respuestas?.respoTres[0]}
-          </Button>
-        </div>
-        <div>
-          <Button
-            value={respuestas?.respoCuatro[1]}
-            bordered
-            color={bot4 ? "success" : "grey"}
-            name='4'
-            onClick={() => corroborador(respuestas?.respoCuatro)}>
-            {respuestas?.respoCuatro[0]}
-          </Button>
-        </div>
-      </div>
+      <Grid.Container css={{mb:"30px",mt:"50px"}}>
+        <Grid.Container xs={12} justify="center">
+          <Grid xs={12} justify="center">
+            <Text className="frase" h2>
+              pregunta numero {numPregunta}
+            </Text>
+          </Grid>
+          <Grid justify="center" xs={12}>
+            <Text h3>{content}</Text>
+          </Grid>
+          <Grid.Container gap={5} justify="center">
+            <Grid >
+              <Button
+                value={respuestas?.respoUno[1]}
+                shadow
+                css={{ bg: "#025a4e", color: "white" }}
+                auto
+                color={bot1 ? "success" : "grey"}
+                name='1'
+                onClick={() => corroborador(respuestas?.respoUno)}>
+                {respuestas?.respoUno[0]}
+              </Button>
+            </Grid>
+            <Grid >
+              <Button
+                value={respuestas?.respoDos[1]}
+                shadow
+                css={{ bg: "#025a4e", color: "white" }}
+                auto
+                color={bot2 ? "success" : "grey"}
+                name='2'
+                onClick={() => corroborador(respuestas?.respoDos)}>
+                {respuestas?.respoDos[0]}
+              </Button>
+            </Grid>
+            <Grid >
+              <Button
+                value={respuestas?.respoTres[1]}
+                shadow
+                css={{ bg: "#025a4e", color: "white" }}
+                auto
+                color={bot3 ? "success" : "grey"}
+                name='3'
+                onClick={() => corroborador(respuestas?.respoTres)}>
+                {respuestas?.respoTres[0]}
+              </Button>
+            </Grid>
+            <Grid>
+              <Button
+                value={respuestas?.respoCuatro[1]}
+                shadow
+                css={{ bg: "#025a4e", color: "white" }}
+                auto
+                color={bot4 ? "success" : "grey"}
+                name='4'
+                onClick={() => corroborador(respuestas?.respoCuatro)}>
+                {respuestas?.respoCuatro[0]}
+              </Button>
+            </Grid>
+          </Grid.Container>
+        </Grid.Container>
+      </Grid.Container>
     </div>
   );
 }
