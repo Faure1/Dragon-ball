c2890659e72fec3a11a35495e64c431aa1c5656a
nicop1234 <101873030+nicop1234@users.noreply.github.com>
2023-05-23 10:17:10 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -3,7 +3,77 @@
 const Asks = [
   {
     numPregunta: "1",
-    content: "lo que vaya a ir dentro",
+    content: "¿Qué es un algoritmo?",
+    repuestas: {
+      respoUno: ["Una secuencia de instrucciones", true, "but1"],
+
+      respoDos: ["Codigos de propgramacion", false, "but2"],
+
+      respoTres: ["Una App", false, "but3"],
+
+      respoCuatro: ["Ninguna de las anteriores", false, "but4"],
+    },
+    respoCorrecta: "but1"
+  },
+
+
+  {
+    numPregunta: "2",
+    content: "¿Qué es el deep learning?",
+    repuestas: {
+      respoUno: ["La opcion 2 y 3 son correctas", true, "but1"],
+
+      respoDos: ["Redes neuronales artificiales", false, "but2"],
+
+      respoTres: ["Aprender y realizar tareas", false, "but3"],
+
+      respoCuatro: ["Investigaciones sobre la IA", false, "but4"],
+    },
+    respoCorrecta: "but1"
+  },
+    {
+    numPregunta: "3",
+    content: "La IA esta compuesta por...",
+    repuestas: {
+      respoUno: ["Machine Learning", false, "but1"],
+
+      respoDos: ["Maquinas", false, "but2"],
+
+      respoTres: ["Deep Learnin y machine learning", true, "but3"],
+
+      respoCuatro: ["Fuentes de Internet", false, "but4"],
+    },
+    respoCorrecta: "but3"
+  },
+      {
+    numPregunta: "4",
+    content: "La IA se puede interpretar por...",
+    repuestas: {
+      respoUno: ["Sistema Informatico", false, "but1"],
+
+      respoDos: ["Opcion1 y 3 son correctas", true, "but2"],
+
+      respoTres: ["Procesos de inteligencia", false, "but3"],
+
+      respoCuatro: ["Redes sociales", false, "but4"],
+    },
+    respoCorrecta: "but2"
+  },
+      numPregunta: "5",
+    content: "Los impactos de la IA...",
+    repuestas: {
+      respoUno: ["Falta de etica", false, "but1"],
+
+      respoDos: ["Vulnerabilidad de internet", true, "but2"],
+
+      respoTres: ["Destruccion de empleos", false, "but3"],
+
+      respoCuatro: ["Todas son correctas", true, "but4"],
+    },
+    respoCorrecta: "but4"
+  },
+          numPregunta: "6",
+    content: "¿Cual de estos ejemplos es un uso cotidiano?",
     repuestas: {
       respoUno: ["respuesta1", false, "but1"],
 
@@ -14,22 +84,6 @@ const Asks = [
       respoCuatro: ["respuesta4", false, "but4"],
     },
     respoCorrecta: "but2"
-  },
-
-
-  {
-    numPregunta: "2",
-    content: "lo que vaya a ir dentro",
-    repuestas: {
-      respoUno: ["respuesta1", true, "but1"],
-
-      respoDos: ["respuesta2", false, "but2"],
-
-      respoTres: ["respuesta3", false, "but3"],
-
-      respoCuatro: ["respuesta4", false, "but4"],
-    },
-    respoCorrecta: "but1"
   },
 ];
 
